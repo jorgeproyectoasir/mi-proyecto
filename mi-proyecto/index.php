4e259e4fc7f1436a92d680fe39ba713233082a60
jorgeproyectoasir <fpb1.jorge.junca@gmail.com>
2025-05-04 20:39:04 +0200

    Cambios realizados para probar el proyecto

--- mi-proyecto/index.php
+++ mi-proyecto/index.php
@@ -1,3 +1,3 @@
 <?php include "includes/header.php"; ?>
-<h1>Bienvenido a mi proyecto ASIR GUARRILLAS</h1>
+<h1>Bienvenido a mi proyecto ASIR Marineros</h1>
 <a href="login.php">Iniciar sesión</a> | <a href="registro.php">Registrarse</a>
