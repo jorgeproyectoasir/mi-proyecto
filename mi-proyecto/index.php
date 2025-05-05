eebb7604a7acae6245aa00ef10aceaa50f2ed26c
jorgeproyectoasir <fpb1.jorge.junca@gmail.com>
2025-05-05 16:32:13 +0200

    Prueba del 4 de Mayo

--- mi-proyecto/index.php
+++ mi-proyecto/index.php
@@ -1,3 +1,5 @@
 <?php include "includes/header.php"; ?>
+<?php echo "Última actualización: 4 de mayo, 2025"; ?>
+
 <h1>Bienvenido a mi proyecto ASIR Marineros</h1>
 <a href="login.php">Iniciar sesión</a> | <a href="registro.php">Registrarse</a>
